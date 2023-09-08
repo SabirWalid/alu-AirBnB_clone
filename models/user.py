123cf4d2ff8eec8c241b56cc3fb2dcf6f8eaa593
SabirWalid <s.abdurahma@alustudent.com>
2023-09-08 20:19:26 +0200

    User datails

---
+++ models/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Class representing a user"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Details of a user"""
+    email = ""
+    password = ""
+    first_name = ""
+    second_name = ""
+
+    """def __init__(self, *args, **kwargs):
+        Initialize a User instance
+        super().__init__(*args, **kwargs)"""
