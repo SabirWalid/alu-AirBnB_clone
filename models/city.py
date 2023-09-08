3d8825597b9ed3490f9a017d8b076cb9c93562a6
SabirWalid <s.abdurahma@alustudent.com>
2023-09-08 23:17:57 +0200

    city.py

---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class city that inherits from the BaseModel class"""
+
+from models import BaseModel
+
+
+class City(BaseModel):
+    """City class"""
+
+    state_id = ""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
