54ce9c2fb050b09ebe1efe4b560f9a7bce596edf
SabirWalid <s.abdurahma@alustudent.com>
2023-09-08 20:27:53 +0200

    the storage file

---
+++ models/engine/file_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import json
+from os import path
+
+
+class FileStorage:
+    """ file storage class"""
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ returns : dictionary """
+        return self.__objects
+
+    def new(self, obj):
+        """ set object with key """
+        self.__objects[obj.__class__.__name__ + '.' + obj.id] = obj
+
+    def save(self):
+        """ serializes to json file """
+        temp = {}
+        for key in self.__objects:
+            temp[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, "w+", encoding='utf-8') as out_file:
+            json.dump(temp, out_file)
+
+    def reload(self):
+        """ deserializes json to file """
+        from models.base_model import BaseModel
+        from models.user import Place
+        from models.city import Review
+        from models.state import State
+        from models.place import User
+        from models.review import Amenity
+        from models.amenity import City
+        if path.exists(self.__file_path):
+            with open(self.__file_path, "r", encoding='utf-8') as in_file:
+                dataset = json.load(in_file)
+                for data in dataset.values():
+                    name_of_class = data['__class__']
+                    self.new(eval(name_of_class + "(**" + str(data) + ")"))
+
