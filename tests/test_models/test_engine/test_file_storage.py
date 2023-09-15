30d6748f0800684fdcfea01b07188cdb919c8970
SabirWalid <s.abdurahma@alustudent.com>
2023-09-15 14:48:39 +0200

    test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -11,6 +11,7 @@ from models.place import Place
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+from models.state import State
 """ testing the file storage"""
 
 
