e1360a10017c01bf1abe7b7220bbf293145cbc48
SabirWalid <s.abdurahma@alustudent.com>
2023-09-15 14:55:02 +0200

    test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -79,7 +79,7 @@ class TestCaseFileStorage(unittest.TestCase):
         self.assertIn("Review." + rv.id, objs)
 
     def test_reload_no_file(self):
-        self.assertRaises(FileNotFoundError, models.storage.reload())
+        self.assertRaises(FileNotFoundError, models.storage.reload)
 
     def test_reload_with_arg(self):
         with self.assertRaises(TypeError):
